4fa1611fe2583ec22c6927abdf583a2e1a6c597a
NextGen-Innovators-Club <nextgen.pcu25@gmail.com>
2025-10-02 23:20:15 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-export default {
-  base: '/', // remove subfolder reference
-}
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+})
 // // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
