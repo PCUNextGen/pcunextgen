11e0b7528339b4fe5ecb8a8ae8784269c9feded5
Varun Sahu <varunvs7692@gmail.com>
2025-10-03 23:26:15 +0530

    Consolidate frontend and Frontend directories into single Frontend directory

--- Frontend/client/pages/RegisterNow.tsx
+++
@@ -1,5 +0,0 @@
-import EventRegister from '../components/sections/EventRegister';
-
-export default function RegisterNowPage() {
-  return <EventRegister />;
-}
--- Frontend/src/pages/RegisterNow.jsx
+++
@@ -1,5 +0,0 @@
-import EventRegister from '../components/sections/EventRegister.jsx';
-
-export default function RegisterNowPage() {
-  return <EventRegister />;
-}
