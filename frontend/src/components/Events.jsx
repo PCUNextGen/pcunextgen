75ea63d75cdb055f298000a1969365c4105eb906
Varun Sahu <varunvs7692@gmail.com>
2025-10-03 14:00:51 +0530

    Update Events.jsx

--- frontend/src/components/Events.jsx
+++ frontend/src/components/Events.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+// eslint-disable-next-line no-unused-vars
 import { motion, useInView } from 'framer-motion';
 import EventRegister from './sections/EventRegister';
 
